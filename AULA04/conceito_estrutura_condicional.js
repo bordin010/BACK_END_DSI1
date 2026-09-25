98dca1a20745e64be9a5856b4726e329731108f2
bordin010 <enzodominone@gmail.com>
2026-09-25 15:28:06 -0300

    Quarta Aula JavaScript

---
+++ AULA04/conceito_estrutura_condicional.js
@@ -0,0 +1,26 @@
+// Estruturas Condicionais (Tomando Decisões)
+// As estruturas condicionais permitem executar diferentes blocos de código dependendo de uma condição.
+
+// If/Else - Condicionais
+// If - Verifica se uma condição é verdadeira e executa o código dentro dele, se a condição for falsa, o ELSE pode executar outro bloco de código
+
+let idade = 12;
+
+if (idade >= 18) {
+    console.log("Você é maior de idade")
+} else {
+    console.log("Você é menor de idade");
+
+}
+
+// IF, ELSE, IF, ELSE (Multiplas Condições)
+let idade2 = 19;
+
+if (idade2 < 12) {
+    console.log("Você é uma criança 👶")
+} else if (idade2 < 18) {
+    console.log("Você é um adolescente 👦")
+} else {
+    console.log("Você é um adulto 🏆")
+
+}
